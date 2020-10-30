150aac603c1f1aa7614a6e228da283a19de265ff
Alan Gutierrez <alan@prettyrobots.com>
2020-10-30 03:19:55 -0500

    Use anonymous function for merge compare.
    Looks nicer.
    
    Closes #109.

--- merge.js
+++ merge.js
@@ -1,13 +1,12 @@
 const assert = require('assert')
 
 module.exports = function (comparator, direction) {
-    function compare (left, right) {
-        return comparator(left.inner[left.index].key, right.inner[right.index].key) * direction
-    }
     return function (iterators, consume) {
         const gathered = []
         do {
-            iterators.sort(compare)
+            iterators.sort((left, right) => {
+                return comparator(left.inner[left.index].key, right.inner[right.index].key) * direction
+            })
             gathered.push(iterators[0].inner[iterators[0].index++])
         } while (iterators[0].inner.length != iterators[0].index)
         assert.notEqual(gathered.length, 0)
